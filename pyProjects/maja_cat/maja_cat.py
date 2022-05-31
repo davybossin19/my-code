60baf7b27e51077bb0aad2df25e662702ec80175
Davy Bossin <davybossin@gmail.com>
2022-05-30 21:46:11 -0700

    Maja Cat

---
+++ pyProjects/maja_cat/maja_cat.py
@@ -0,0 +1,29 @@
+import os, time
+
+os.system('clear')
+
+cats = ['cat1.txt', 'cat2.txt', 'cat3.txt']
+frames = []
+
+for cat in cats:
+    with open(cat, 'r', encoding='utf8') as file:
+        frames.append(file.readlines())
+
+count = 0
+print(frames[0])
+os.system('clear')
+while count < 35:
+    frame = frames[count % 3]
+    for i, line in enumerate(frame):
+        frame[i] = ' '*10 + line
+    print("".join(frame))
+    count += 1
+    time.sleep(0.2)
+    os.system('clear')
+
+with open('banner.txt', 'r', encoding = 'utf8') as banner:
+    frames.append(banner.readlines())
+
+print("".join(frames[-1]))
+time.sleep(2)
+os.system('clear')
